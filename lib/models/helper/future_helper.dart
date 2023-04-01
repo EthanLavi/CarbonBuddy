96ccaba6981a0926d95570c09696b268d997526a
EthanLavi <89608076+EthanLavi@users.noreply.github.com>
2023-04-01 14:31:35 -0400

    Adding UI structure

---
+++ lib/models/helper/future_helper.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// A class for generating the future builer.
+/// Will show a circular progress indicator while waiting for data to be fetched. Then will show child.
+/// - [T] is the type of the data to fetch
+/// - [future] is the future we want to wait for
+/// - [child] is a lambda function that takes one variable (the data returned from the future) and is tasked with displaying the data after it has loaded
+FutureBuilder waitWhileQuerying<T>({required Future<T> future, required Widget Function(T data) child}) {
+  return FutureBuilder(
+    future: future, // load the future into the future builder
+    builder: (context, snapshot) {
+      if (snapshot.hasError) {
+        // If snapshot has an error, show error 
+        return Text("Error: ${snapshot.error}", style: const TextStyle(color: Colors.red));
+      } else if (!snapshot.hasData) {
+        // If snapshot is still loading, show progress indicator
+        return const Center(child: CircularProgressIndicator());
+      }
+
+      // If snapshot is finished loading, show child
+      return child(snapshot.data as T);
+    },
+  );
+}
